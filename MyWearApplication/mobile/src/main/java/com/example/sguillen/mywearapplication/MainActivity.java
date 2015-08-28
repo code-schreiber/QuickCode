d6dfae19b6f8cd77d9f9aeb7e7af32c993d70a7d
sebasguillen <sebasguillen@gmail.com>
2015-08-28 11:28:36 +0200

    first commit

---
+++ MyWearApplication/mobile/src/main/java/com/example/sguillen/mywearapplication/MainActivity.java
@@ -0,0 +1,366 @@
+package com.example.sguillen.mywearapplication;
+
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+
+public class MainActivity extends Activity implements
+        NavigationDrawerFragment.NavigationDrawerCallbacks,
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final String COUNT_KEY = "TODO_FIND_A_WAY_TO_READ_FROM_ONE_SOURCE";//TODO
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume ");
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected " + bundle);
+        Log.d(TAG, "mGoogleApiClient " + mGoogleApiClient);
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        Log.d(TAG, dataEventBuffer + " onDataChanged");
+        for (DataEvent event : dataEventBuffer) {
+            Log.d(TAG, "event.getType() " + event.getType());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended " + i);
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed " + connectionResult);
+    }
+
+    // Create a data map and put data in it
+    private void increaseCounter(int count) {
+        String path = "/count";
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create(path);
+        // For sending text 100KB of data is 102400 characters
+        putDataMapReq.getDataMap().putInt(COUNT_KEY, count);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+        Log.d(TAG, count + " putDataItem " + putDataReq);
+        // FIXME devices not connected http://stackoverflow.com/questions/22524760/not-able-to-connect-android-wear-emulator-with-device
+//        React to when it sends the data
+// developer.android.com/training/wearables/data-layer/events.html#Wait
+        pendingResult.addBatchCallback(new PendingResult.BatchCallback() {
+            @Override
+            public void zzs(Status status) {
+                Log.d(TAG, "zzs " + status);
+            }
+        });
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private RecyclerView mRecyclerView;
+        private LinearLayoutManager mLayoutManager;
+        private MyCustomAdapter mAdapter;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.fragment_main_recycler_view);
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            mLayoutManager = new LinearLayoutManager(getActivity());
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            String[] myDataset = {"uno", "dos", "three"};
+            mAdapter = new MyCustomAdapter(myDataset);
+            mRecyclerView.setAdapter(mAdapter);
+
+//            FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fragment_main_floating_action_button);
+//            fab.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    //TODO see http://stackoverflow.com/questions/24451026/android-l-floating-action-button-fab/24548910#24548910
+//                    ((MainActivity)getActivity()).increaseCounter();
+//                }
+//            });
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+
+
+        private class MyCustomAdapter extends RecyclerView.Adapter<MyCustomAdapter.ViewHolder> {
+
+            private String[] mDataset;
+
+            // Provide a reference to the views for each data item
+            // Complex data items may need more than one view per item, and
+            // you provide access to all the views for a data item in a view holder
+            public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {//FIXME make static
+
+
+                private final IMyViewHolderClicks mListener;
+
+                // each data item is just a string in this case
+                public TextView mTextView;
+
+                public ViewHolder(View v, IMyViewHolderClicks listener) {
+                    super(v);
+                    this.mListener = listener;
+                    this.mTextView = (TextView) v.findViewById(R.id.item_list_text_view);
+                }
+
+                @Override
+                public void onClick(View v) {
+                    // TODO make this get triggered
+                    Log.d(TAG, "onClick" + this.mTextView.getText());
+                    boolean a = true;
+                    if (a){
+                        mListener.onTomato(v);
+                    } else {
+                        mListener.onPotato(v);
+                    }
+                }
+
+
+
+            }
+
+
+
+            // Provide a suitable constructor (depends on the kind of dataset)
+            public MyCustomAdapter(String[] myDataset) {
+                mDataset = myDataset;
+            }
+
+            // Create new views (invoked by the layout manager)
+            @Override
+            public MyCustomAdapter.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+                // create a new view
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+                IMyViewHolderClicks listener = new IMyViewHolderClicks() {
+                    @Override
+                    public void onPotato(View caller) {
+                        // Here just as an example
+                    }
+
+                    @Override
+                    public void onTomato(View caller) {
+                        int index = parent.indexOfChild(caller);
+                        View child = parent.getChildAt(index);
+                        // TODO use child, see what type it is
+                        ((MainActivity)getActivity()).increaseCounter(index);
+                    }
+                };
+                ViewHolder vh = new ViewHolder(v, listener);
+                return vh;
+            }
+
+            // Replace the contents of a view (invoked by the layout manager)
+            @Override
+            public void onBindViewHolder(ViewHolder holder, int position) {
+                // - get element from your dataset at this position
+                // - replace the contents of the view with that element
+                holder.mTextView.setText(mDataset[position]);
+            }
+
+            // Return the size of your dataset (invoked by the layout manager)
+            @Override
+            public int getItemCount() {
+                return mDataset.length;
+            }
+
+
+        }
+
+        interface IMyViewHolderClicks {//FIXME make static and put in view holder
+            public void onPotato(View caller);
+            public void onTomato(View callerImage);
+        }
+
+    }
+
+}
+
+
---
+++ MyWearApplication/wear/src/main/java/com/example/sguillen/mywearapplication/MainWearActivity.java
@@ -0,0 +1,123 @@
+package com.example.sguillen.mywearapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainWearActivity extends Activity implements
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private final static String TAG = MainWearActivity.class.getSimpleName();
+
+    private static final String COUNT_KEY = "TODO_FIND_A_WAY_TO_READ_FROM_ONE_SOURCE";
+
+    private TextView mTextView;
+
+    private GoogleApiClient mGoogleApiClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_wear);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume ");
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected " + bundle);
+        Log.d(TAG, "mGoogleApiClient " + mGoogleApiClient);
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended " + i);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed " + connectionResult);
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        Log.d(TAG, dataEvents + " onDataChanged");
+        for (DataEvent event : dataEvents) {
+            Log.d(TAG, "event.getType() " + event.getType());
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // DataItem changed
+                DataItem item = event.getDataItem();
+                String path = "/count";
+                if (item.getUri().getPath().compareTo(path) == 0) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    updateCount(dataMap.getInt(COUNT_KEY));
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                Log.d(TAG, "DataItem deleted ");
+                // DataItem deleted
+            }
+        }
+    }
+
+    // Our method to update the count
+    private void updateCount(final int c) {
+        //TODO
+        Log.d(TAG, "Got " + c);
+
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+//stuff that updates ui
+                mTextView.setText("Got " + c);
+            }
+        });
+    }
+
+
+}
