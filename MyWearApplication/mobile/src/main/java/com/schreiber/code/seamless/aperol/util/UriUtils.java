436b7090b79089fd0c44429ae69a1cf59cb6fea2
code-schreiber <sebas@Speedy.fritz.box>
2017-02-22 21:59:18 +0100

    Item click stub logic

---
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/util/UriUtils.java
@@ -0,0 +1,135 @@
+package com.schreiber.code.seamless.aperol.util;
+
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
+import android.support.annotation.CheckResult;
+import android.support.annotation.Nullable;
+
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class UriUtils {
+
+    private UriUtils() {
+        // Hide utility class constructor
+    }
+
+    @CheckResult
+    public static String readTextFromUri(ContentResolver contentResolver, Uri uri) {
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStream inputStream = null;
+        try {
+            inputStream = contentResolver.openInputStream(uri);
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+            }
+        } catch (IOException e) {
+            Logger.logException(e);
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                Logger.logException(e);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @Nullable
+    @CheckResult
+    public static Bitmap getBitmapFromUri(ContentResolver contentResolver, Uri uri) {
+        ParcelFileDescriptor parcelFileDescriptor = null;
+        try {
+            parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r");
+            if (parcelFileDescriptor != null) {
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                return BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            }
+        } catch (FileNotFoundException e) {
+            Logger.logException(e);
+        } finally {
+            try {
+                if (parcelFileDescriptor != null) {
+                    parcelFileDescriptor.close();
+                }
+            } catch (IOException e) {
+                Logger.logException(e);
+            }
+        }
+        return null;
+    }
+
+    @CheckResult
+    public static String getDisplayName(ContentResolver contentResolver, Uri uri) {
+
+        // The query, since it only applies to a single document, will only return
+        // one row. There's no need to filter, sort, or select fields, since we want
+        // all fields for one document.
+        Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
+        if (cursor != null)
+            try {
+                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+                // "if there's anything to look at, look at it" conditionals.
+                if (cursor.moveToFirst()) {
+
+                    // Note it's called "Display Name".  This is
+                    // provider-specific, and might not necessarily be the file name.
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    return cursor.getString(nameIndex);
+                }
+            } finally {
+                cursor.close();
+            }
+        return "";
+    }
+
+    @CheckResult
+    public static String getSize(ContentResolver contentResolver, Uri uri) {
+
+        // The query, since it only applies to a single document, will only return
+        // one row. There's no need to filter, sort, or select fields, since we want
+        // all fields for one document.
+        Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
+        if (cursor != null)
+            try {
+                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+                // "if there's anything to look at, look at it" conditionals.
+                if (cursor.moveToFirst()) {
+                    int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                    // If the size is unknown, the value stored is null.  But since an
+                    // int can't be null in Java, the behavior is implementation-specific,
+                    // which is just a fancy term for "unpredictable".  So as
+                    // a rule, check if it's null before assigning to an int.  This will
+                    // happen often:  The storage API allows for remote files, whose
+                    // size might not be locally known.
+                    if (!cursor.isNull(sizeIndex)) {
+                        // Technically the column stores an int, but cursor.getString()
+                        // will do the conversion automatically.
+                        return cursor.getString(sizeIndex) + " bytes";
+                    }
+                }
+            } finally {
+                cursor.close();
+            }
+        return "Unknown";
+    }
+
+
+}
--- MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/MainActivityFragment.java
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/MainActivityFragment.java
@@ -1,44 +1,34 @@
 package com.schreiber.code.seamless.aperol.view.main;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.provider.OpenableColumns;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 
 import com.schreiber.code.seamless.aperol.R;
 import com.schreiber.code.seamless.aperol.model.ListItem;
 import com.schreiber.code.seamless.aperol.util.Logger;
+import com.schreiber.code.seamless.aperol.util.UriUtils;
 import com.schreiber.code.seamless.aperol.view.common.view.OnViewClickedListener;
 
-import java.io.BufferedReader;
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment implements OnViewClickedListener {
-
-    private final static String TAG = "MainActivityFragment";
 
     /**
      * The fragment argument representing the section number for this
@@ -50,9 +40,6 @@ public class MainActivityFragment extends Fragment implements OnViewClickedListe
     private MyCustomAdapter adapter;
 
     private static final int READ_REQUEST_CODE = 111;
-    private static final int READ_PDF_REQUEST_CODE = 222;
-    private static final int READ_IMAGE_REQUEST_CODE = 333;
-    private static final int READ_TEXT_REQUEST_CODE = 444;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -83,7 +70,7 @@ public class MainActivityFragment extends Fragment implements OnViewClickedListe
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
-        List<ListItem> data = Arrays.asList(new ListItem("uno"), new ListItem("dos"), new ListItem("three"));
+        List<ListItem> data = new ArrayList<>();
         adapter = new MyCustomAdapter(data, this);
         recyclerView.setAdapter(adapter);
 
@@ -125,147 +112,50 @@ public class MainActivityFragment extends Fragment implements OnViewClickedListe
                 // Pull that URI using resultData.getData().
                 if (resultData != null) {
                     Uri uri = resultData.getData();
-                    String s = "File name " + getDisplayName(uri) +
-                            "\ntype " + requestCode +
-                            "\nsize " + getSize(uri) +
-                            "\nUri " + uri.toString();
-                    adapter.addAnItem(new ListItem(s));
-                }
-            } else if (requestCode == READ_PDF_REQUEST_CODE)
-                showSnack("requestCode: " + requestCode);
-            else if (requestCode == READ_IMAGE_REQUEST_CODE)
-                showSnack("requestCode: " + requestCode);
-            else showSnack("resultCode: " + resultCode);
+                    if (uri != null) {
+                        adapter.addAnItem(itemFromUri(uri));
+                    }
+                } else showSnack("resultData: " + resultData);
+            } else showSnack("requestCode: " + requestCode);
+        else showSnack("resultCode: " + resultCode);
+    }
+
+    @NonNull
+    private ListItem itemFromUri(@NonNull Uri uri) {
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        String filename = UriUtils.getDisplayName(contentResolver, uri);
+        String type = contentResolver.getType(uri);
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        String mimeType = mime.getExtensionFromMimeType(type);
+        String size = UriUtils.getSize(contentResolver, uri);
+        return ListItem.create(filename, type, mimeType, size, uri);
     }
 
     private void showSnack(String m) {
-        Log.i(TAG, m);
+        Logger.logInfo(m);
         Snackbar.make(recyclerView, m, Snackbar.LENGTH_SHORT).show();
-    }
-
-    private String readTextFromUri(Uri uri) {
-        StringBuilder stringBuilder = new StringBuilder();
-        InputStream inputStream = null;
-        try {
-            inputStream = getActivity().getContentResolver().openInputStream(uri);
-            if (inputStream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
-                }
-            }
-        } catch (IOException e) {
-            Logger.logException(e);
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (IOException e) {
-                Logger.logException(e);
-            }
-        }
-        return stringBuilder.toString();
-    }
-
-    @Nullable
-    private Bitmap getBitmapFromUri(Uri uri) {
-        ParcelFileDescriptor parcelFileDescriptor = null;
-        try {
-            parcelFileDescriptor = getActivity().getContentResolver().openFileDescriptor(uri, "r");
-            if (parcelFileDescriptor != null) {
-                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-                return BitmapFactory.decodeFileDescriptor(fileDescriptor);
-            }
-        } catch (FileNotFoundException e) {
-            Logger.logException(e);
-        } finally {
-            try {
-                if (parcelFileDescriptor != null) {
-                    parcelFileDescriptor.close();
-                }
-            } catch (IOException e) {
-                Logger.logException(e);
-            }
-        }
-        return null;
-    }
-
-    public String getDisplayName(Uri uri) {
-
-        // The query, since it only applies to a single document, will only return
-        // one row. There's no need to filter, sort, or select fields, since we want
-        // all fields for one document.
-        Cursor cursor = getActivity().getContentResolver()
-                .query(uri, null, null, null, null, null);
-        if (cursor != null)
-            try {
-                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-                // "if there's anything to look at, look at it" conditionals.
-                if (cursor.moveToFirst()) {
-
-                    // Note it's called "Display Name".  This is
-                    // provider-specific, and might not necessarily be the file name.
-                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                    return cursor.getString(nameIndex);
-                }
-            } finally {
-                cursor.close();
-            }
-        return "";
-    }
-
-    public String getSize(Uri uri) {
-
-        // The query, since it only applies to a single document, will only return
-        // one row. There's no need to filter, sort, or select fields, since we want
-        // all fields for one document.
-        Cursor cursor = getActivity().getContentResolver()
-                .query(uri, null, null, null, null, null);
-        if (cursor != null)
-            try {
-                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-                // "if there's anything to look at, look at it" conditionals.
-                if (cursor.moveToFirst()) {
-                    int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
-                    // If the size is unknown, the value stored is null.  But since an
-                    // int can't be null in Java, the behavior is implementation-specific,
-                    // which is just a fancy term for "unpredictable".  So as
-                    // a rule, check if it's null before assigning to an int.  This will
-                    // happen often:  The storage API allows for remote files, whose
-                    // size might not be locally known.
-                    if (!cursor.isNull(sizeIndex)) {
-                        // Technically the column stores an int, but cursor.getString()
-                        // will do the conversion automatically.
-                        return cursor.getString(sizeIndex) + " bytes";
-                    }
-                }
-            } finally {
-                cursor.close();
-            }
-        return "Unknown";
     }
 
     @Override
     public void onItemClicked(ListItem item) {
-        Uri uri = Uri.parse(item.s);
-        if (item.s.contains("\ntype " + READ_REQUEST_CODE)) {
+        item.mimeType();
+        if (item.type().equals("a type")) {
             // Handle file
-        } else if (item.s.contains("\ntype " + READ_IMAGE_REQUEST_CODE)) {
+
+        } else if (item.type().equals("\ntype ")) {
             // Handle image file
-            Bitmap b = getBitmapFromUri(uri);
+            Bitmap b = UriUtils.getBitmapFromUri(getActivity().getContentResolver(), item.uri());
             // TODO show(b);
-        } else if (item.s.contains("\ntype " + READ_PDF_REQUEST_CODE)) {
+        } else if (item.type().equals("\ntype ")) {
             // Handle PDF file
-            // TODO PDF pdf = getPdfFromUri(uri);
+            // TODO PDF pdf = getPdfFromUri(item.uri());
             // TODO show(pdf);
-        } else if (item.s.contains("\ntype " + READ_TEXT_REQUEST_CODE)) {
+        } else if (item.type().equals("\ntype ")) {
             // Handle Text file
-            String text = readTextFromUri(uri);
+            String text = UriUtils.readTextFromUri(getActivity().getContentResolver(), item.uri());
             showSnack(text);
         } else {
-            showSnack("No known type: " + item.s);
+            showSnack("No known type: " + item.type());
         }
     }
 
