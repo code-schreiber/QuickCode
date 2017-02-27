f2a7d6c6537c4ae13cc679f35ea4f1b5c1f59ce1
code-schreiber <sebas@Speedy.fritz.box>
2017-02-27 21:35:40 +0100

    Cleanup

--- MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/util/Logger.java
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/util/Logger.java
@@ -52,5 +52,4 @@ public class Logger {
         return isMessageOk;
     }
 
-
 }
--- MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/MainActivityFragment.java
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -21,37 +22,21 @@ import com.schreiber.code.seamless.aperol.model.ListItem;
 import com.schreiber.code.seamless.aperol.util.Logger;
 import com.schreiber.code.seamless.aperol.util.UriUtils;
 import com.schreiber.code.seamless.aperol.view.common.view.OnViewClickedListener;
+import com.shockwave.pdfium.PdfDocument;
+import com.shockwave.pdfium.PdfiumCore;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 public class MainActivityFragment extends Fragment implements OnViewClickedListener {
 
-    /**
-     * The fragment argument representing the section number for this
-     * fragment.
-     */
-    private static final String ARG_SECTION_NUMBER = "section_number";
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private MyCustomAdapter adapter;
 
     private static final int READ_REQUEST_CODE = 111;
-
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
-    public static MainActivityFragment newInstance(int sectionNumber) {
-        MainActivityFragment fragment = new MainActivityFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public MainActivityFragment() {
 
@@ -141,27 +126,90 @@ public class MainActivityFragment extends Fragment implements OnViewClickedListe
 
     @Override
     public void onItemClicked(ListItem item) {
-        switch (item.type()) {
+        Uri uri = item.uri();
+        String type = item.type();
+        handleFile(uri, type);
+    }
+
+    void handleFile(Uri uri, String type) {
+        switch (type) {
             case "application/pdf":
-                // TODO PDF pdf = getPdfFromUri(item.uri());
-                // TODO show(pdf);
+                showPdf(uri);
                 break;
-            case "image/png":
-                Bitmap b = UriUtils.getBitmapFromUri(getActivity().getContentResolver(), item.uri());
-                // TODO show(b);
+            case "image/png":// TODO not only png
+                showImage(uri);
                 break;
             case "whats the type for text?":// TODO
-                String textContent = UriUtils.readTextFromUri(getActivity().getContentResolver(), item.uri());
+                String textContent = UriUtils.readTextFromUri(getActivity().getContentResolver(), uri);
                 showSnack(textContent);
                 break;
             case "application/octet-stream":
-                String text = UriUtils.readTextFromUri(getActivity().getContentResolver(), item.uri());
+                String text = UriUtils.readTextFromUri(getActivity().getContentResolver(), uri);
                 showSnack(text);
                 break;
             default:
-                showSnack("No known type: " + item.type());
+                showSnack("No known type: " + type);
                 break;
         }
+    }
+
+    private void showImage(Uri uri) {
+        Bitmap b = UriUtils.getBitmapFromUri(getActivity().getContentResolver(), uri);
+        // TODO show(b);
+    }
+
+    private void showPdf(Uri uri) {
+//        ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+        try {
+            ParcelFileDescriptor fd = getActivity().getContentResolver().openFileDescriptor(uri, UriUtils.MODE_READ);
+            int pageNum = 0;
+            PdfiumCore pdfiumCore = new PdfiumCore(getActivity());
+
+            PdfDocument pdfDocument = pdfiumCore.newDocument(fd);
+
+            pdfiumCore.openPage(pdfDocument, pageNum);
+
+            int width = pdfiumCore.getPageWidthPoint(pdfDocument, pageNum);
+            int height = pdfiumCore.getPageHeightPoint(pdfDocument, pageNum);
+
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            pdfiumCore.renderPageBitmap(pdfDocument, bitmap, pageNum, 0, 0, width, height);
+
+//            iv.setImageBitmap(bitmap);
+
+            printInfo(pdfiumCore, pdfDocument);
+
+            pdfiumCore.closeDocument(pdfDocument);
+        } catch (IOException e) {
+            Logger.logException(e);
+        }
+    }
+
+    private void printInfo(PdfiumCore core, PdfDocument doc) {
+//        PdfDocument.Meta meta = core.getDocumentMeta(doc);
+//        Log.e(TAG, "title = " + meta.getTitle());
+//        Log.e(TAG, "author = " + meta.getAuthor());
+//        Log.e(TAG, "subject = " + meta.getSubject());
+//        Log.e(TAG, "keywords = " + meta.getKeywords());
+//        Log.e(TAG, "creator = " + meta.getCreator());
+//        Log.e(TAG, "producer = " + meta.getProducer());
+//        Log.e(TAG, "creationDate = " + meta.getCreationDate());
+//        Log.e(TAG, "modDate = " + meta.getModDate());
+//
+//        printBookmarksTree(core.getTableOfContents(doc), "-");
+
+    }
+
+    private void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+//        for (PdfDocument.Bookmark b : tree) {
+//
+//            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+//
+//            if (b.hasChildren()) {
+//                printBookmarksTree(b.getChildren(), sep + "-");
+//            }
+//        }
     }
 
 }
