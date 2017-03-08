109317285153dceac1316d54791a0ba8cf4ad815
code-schreiber <code-schreiber@users.noreply.github.com>
2017-03-08 07:58:58 +0100

    Being more verbose

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 echo "Running travis.sh"
-echo "TRAVIS_TAG: $TRAVIS_TAG"
-echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+echo "travis.sh: TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "travis.sh: TRAVIS_TAG: $TRAVIS_TAG"
+echo "travis.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
+ls
 cd MyWearApplication
-echo "Running gradle build"
+ls
+echo "travis.sh: Running gradle build"
 ./gradlew build
-# echo "Running gradle sonarqube"
+echo "travis.sh: mobile/build/outputs/apk now contains:"
+ls mobile/build/outputs/apk
+# echo "travis.sh: Running gradle sonarqube"
 # ./gradlew sonarqube
 cd ..
 
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "Running fastlane supply -v"
-  fastlane supply -v
-  echo "Running fastlane supply run"
+  echo "travis.sh: Running fastlane supply --version"
+  fastlane supply --version
+  echo "travis.sh: Running fastlane supply run"
   fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-release.apk -a alpha --validate_only true
-  echo "Exiting fastlane supply"
+  echo "travis.sh: Exiting fastlane supply"
   exit $?
 echo "Exiting travis.sh"
 fi
