51d4000d6209b48b8d87591a4037cfc695a3ac11
code-schreiber <code-schreiber@users.noreply.github.com>
2017-03-08 18:37:05 +0100

    Cleanup

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Script should bail on first error
+set -e
+
 echo "Running travis.sh"
 echo "travis.sh: TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "travis.sh: TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
 echo "travis.sh: TRAVIS_TAG: $TRAVIS_TAG"
 echo "travis.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
-ls
 cd MyWearApplication
-ls
 echo "travis.sh: Running gradle build"
 ./gradlew build
 echo "travis.sh: mobile/build/outputs/apk now contains:"
@@ -16,16 +17,18 @@ ls mobile/build/outputs/apk
 # echo "travis.sh: Running gradle sonarqube"
 # ./gradlew sonarqube
 cd ..
-ls
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "travis.sh: Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" == "develop" ]; then
-  echo "travis.sh: Running fastlane supply --version"
+if [ "$TRAVIS_REPO_SLUG" != "code-schreiber/seamless-aperol" ]; then
+  echo "travis.sh: Skipping deployment: wrong repository. Expected code-schreiber/seamless-aperol but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "travis.sh: Skipping deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "develop" ]; then
+  echo "travis.sh: Skipping deployment: wrong branch. Expected develop but was '$TRAVIS_BRANCH'."
+else
+  echo "travis.sh: Running fastlane supply"
   fastlane supply --version
-  echo "travis.sh: Running fastlane supply run"
-  fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-release.apk -a alpha --validate_only true
-  echo "travis.sh: Exiting fastlane supply"
+  fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-release.apk -a alpha
+  echo "travis.sh: Deployed to Google Play"
   exit $?
 fi
 echo "Exiting travis.sh"
