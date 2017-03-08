c98933d6a68371c9e8d96eac5c339b0a8a40f573
code-schreiber <sebas@Speedy.fritz.box>
2017-03-08 17:35:21 +0100

    Work in progress in deploy script

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -2,6 +2,7 @@
 
 echo "Running travis.sh"
 echo "travis.sh: TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "travis.sh: TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
 echo "travis.sh: TRAVIS_TAG: $TRAVIS_TAG"
 echo "travis.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
@@ -17,12 +18,14 @@ ls mobile/build/outputs/apk
 cd ..
 ls
 
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "travis.sh: Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" == "develop" ]; then
   echo "travis.sh: Running fastlane supply --version"
   fastlane supply --version
   echo "travis.sh: Running fastlane supply run"
   fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-release.apk -a alpha --validate_only true
   echo "travis.sh: Exiting fastlane supply"
   exit $?
-echo "Exiting travis.sh"
 fi
+echo "Exiting travis.sh"
