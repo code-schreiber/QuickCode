011372371441c48eecb4ff9c06512a7dff1e7e0f
code-schreiber <sebas@Speedy.fritz.box>
2017-03-14 21:54:29 +0100

    set google support library to “25.2.0"

--- MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/BaseActivity.java
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/view/main/BaseActivity.java
@@ -1,14 +1,52 @@
 package com.schreiber.code.seamless.aperol.view.main;
 
+
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.schreiber.code.seamless.aperol.util.TypeUtils;
+import com.schreiber.code.seamless.aperol.view.common.view.dialog.SimpleDialogFragment;
 
 import timber.log.Timber;
 
 
 public class BaseActivity extends AppCompatActivity {
+
+    void overridePendingTransitionEnter() {
+        // TODO
+// overridePendingTransition(R.anim.slide_in_right, android.R.anim.slide_out_left);
+//
+// <set xmlns:android="http://schemas.android.com/apk/res/android">
+// <translate android:fromXDelta="50%p" android:toXDelta="0"
+// android:duration="@android:integer/config_mediumAnimTime"/>
+// <alpha android:fromAlpha="0.0" android:toAlpha="1.0"
+// android:duration="@android:integer/config_mediumAnimTime" />
+// </set>
+//
+// slide_out_left:
+// <set xmlns:android="http://schemas.android.com/apk/res/android">
+// <translate android:fromXDelta="0" android:toXDelta="-50%p"
+// android:duration="@android:integer/config_mediumAnimTime"/>
+// <alpha android:fromAlpha="1.0" android:toAlpha="0.0"
+// android:duration="@android:integer/config_mediumAnimTime" />
+// </set>
+    }
+
+    void overridePendingTransitionExit() {
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+    void overridePendingTransitionFadeIn() {
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    void overridePendingTransitionFadeOut() {
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    void showSimpleDialog(String message) {
+        showDialog(SimpleDialogFragment.newInstance(message));
+    }
 
     void showDialog(DialogFragment dialog) {
         dialog.show(getSupportFragmentManager(), dialog.toString());
