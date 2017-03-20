bad50e934f40ef7dcdba59a35c726bcc89366222
Sebastian <sebasguillen+github@gmail.com>
2017-03-20 23:05:41 +0100

    Updated apk filename

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -10,12 +10,13 @@ echo "travis.sh: TRAVIS_TAG: $TRAVIS_TAG"
 echo "travis.sh: TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
 cd MyWearApplication
+./gradlew printVersion
 echo "travis.sh: Running gradle build"
 ./gradlew build
-echo "travis.sh: mobile/build/outputs/apk now contains:"
-ls mobile/build/outputs/apk
 echo "travis.sh: Running gradle sonarqube"
 ./gradlew sonarqube
+echo "travis.sh: mobile/build/outputs/apk now contains:"
+ls mobile/build/outputs/apk
 cd ..
 
 if [ "$TRAVIS_REPO_SLUG" != "code-schreiber/seamless-aperol" ]; then
@@ -27,7 +28,7 @@ elif [ "$TRAVIS_BRANCH" != "develop" ]; then
 else
   echo "travis.sh: Running fastlane supply"
   fastlane supply --version
-  fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-release.apk -a alpha
+  fastlane supply run -j "private_key.json" -p com.schreiber.code.seamless.aperol -b MyWearApplication/mobile/build/outputs/apk/mobile-prod-release.apk -a alpha
   echo "travis.sh: Deployed to Google Play"
   exit $?
 fi
