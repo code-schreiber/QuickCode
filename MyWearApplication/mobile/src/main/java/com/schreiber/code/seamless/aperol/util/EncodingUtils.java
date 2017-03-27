92a2f16c22f62d2af722ac57a6f2d1b1bed991cf
code-schreiber <sebasguillen+github@gmail.com>
2017-03-27 20:57:46 +0200

    Fixed: zxing's PDF_417 encoding has a crash bug when charset is not set

--- MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/util/EncodingUtils.java
+++ MyWearApplication/mobile/src/main/java/com/schreiber/code/seamless/aperol/util/EncodingUtils.java
@@ -7,9 +7,6 @@ import android.support.annotation.CheckResult;
 import android.support.annotation.Nullable;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
 import com.schreiber.code.seamless.aperol.model.CodeFileFactory;
 
 
@@ -45,37 +42,7 @@ public class EncodingUtils {
     @Nullable
     @CheckResult
     private static Bitmap encodeAsBitmap(String rawContent, BarcodeFormat format, int width, int height) {
-        try {
-            BitMatrix encodedMatrix = new MultiFormatWriter().encode(rawContent, format, width, height, null);
-            int matrixWidth = encodedMatrix.getWidth();
-            int matrixHeight = encodedMatrix.getHeight();
-            int[] pixels = getPixels(encodedMatrix);
-            Bitmap bitmap = Bitmap.createBitmap(matrixWidth, matrixHeight, Bitmap.Config.ARGB_8888);
-            int x = 0;
-            int y = 0;
-            int offset = 0;
-            bitmap.setPixels(pixels, offset, matrixWidth, x, y, matrixWidth, matrixHeight);
-            return bitmap;
-        } catch (IllegalArgumentException | WriterException e) {
-            Logger.logException(e);
-            return null;
-        }
-    }
-
-    @CheckResult
-    private static int[] getPixels(BitMatrix encodedMatrix) {
-        int white = 0xFFFFFFFF;
-        int black = 0xFF000000;
-        int width = encodedMatrix.getWidth();
-        int height = encodedMatrix.getHeight();
-        int[] pixels = new int[width * height];
-        for (int y = 0; y < height; y++) {
-            int offset = y * width;
-            for (int x = 0; x < width; x++) {
-                pixels[offset + x] = encodedMatrix.get(x, y) ? black : white;
-            }
-        }
-        return pixels;
+        return new Encoder().encodeAsBitmap(rawContent, format, width, height);
     }
 
 }
