bffe9995e75014d1921327513ec279e1ce94ec3b
code-schreiber <sebasguillen+github@gmail.com>
2017-12-18 17:48:36 +0100

    Using multidex correctly

--- QuickCode/mobile/src/main/java/com/toolslab/quickcode/QuickCodeApp.java
+++ QuickCode/mobile/src/main/java/com/toolslab/quickcode/QuickCodeApp.java
@@ -1,6 +1,8 @@
 package com.toolslab.quickcode;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.squareup.leakcanary.LeakCanary;
 
@@ -21,6 +23,12 @@ public class QuickCodeApp extends Application {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
 }
