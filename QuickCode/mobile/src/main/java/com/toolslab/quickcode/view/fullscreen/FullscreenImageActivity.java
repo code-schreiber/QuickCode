aab959c91697ab6fba23eb82811029a7ac6245a4
code-schreiber <sebasguillen+github@gmail.com>
2017-12-23 14:53:29 +0100

    Clean dead code

--- QuickCode/mobile/src/main/java/com/toolslab/quickcode/view/fullscreen/FullscreenImageActivity.java
+++ QuickCode/mobile/src/main/java/com/toolslab/quickcode/view/fullscreen/FullscreenImageActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.util.Linkify;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -32,21 +31,12 @@ public class FullscreenImageActivity extends BaseActivity {
     private static final String CODE_FILE_ID = "CODE_FILE_ID";
 
     /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-    /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
+    private static final int DELAY_MILLIS = 100;
+
     private final Handler mHideHandler = new Handler();
     private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
@@ -73,20 +63,6 @@ public class FullscreenImageActivity extends BaseActivity {
         @Override
         public void run() {
             hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
         }
     };
 
@@ -126,8 +102,10 @@ public class FullscreenImageActivity extends BaseActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 CodeFileViewModel codeFileViewModel = DatabaseReferenceWrapper.getCodeFileFromDataSnapshot(dataSnapshot);
-                setTitle(codeFileViewModel.getDisplayName());
-                binding.setCodeFileViewModel(codeFileViewModel);
+                if (codeFileViewModel != null) {
+                    setTitle(codeFileViewModel.getDisplayName());
+                    binding.setCodeFileViewModel(codeFileViewModel);
+                }
             }
 
             @Override
@@ -163,7 +141,7 @@ public class FullscreenImageActivity extends BaseActivity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
-        delayedHide(100);
+        delayedHide();
     }
 
     @Override
@@ -223,8 +201,8 @@ public class FullscreenImageActivity extends BaseActivity {
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
      */
-    private void delayedHide(int delayMillis) {
+    private void delayedHide() {
         mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        mHideHandler.postDelayed(mHideRunnable, DELAY_MILLIS);
     }
 }
