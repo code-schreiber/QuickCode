57c08b1a5b5a87f6bbde392acd507e46665447bd
code-schreiber <code-schreiber@users.noreply.github.com>
2018-01-24 12:18:51 +0100

    Printing version just before "Deployed to Google Play"

--- deploy.sh
+++ deploy.sh
@@ -35,6 +35,8 @@ else
   echo "deploy.sh: Running fastlane supply"
   fastlane supply --version
   fastlane supply run --json_key dev-console-api-private-key.json --package_name com.toolslab.quickcode --apk QuickCode/mobile/build/outputs/apk/release/mobile-release.apk --track alpha
+  cd QuickCode
+  ./gradlew printVersion
   echo "deploy.sh: Deployed to Google Play"
   exit $?
 fi
