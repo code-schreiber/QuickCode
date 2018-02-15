addd8c0a5487a717826311d0990481dd4f6901f1
code-schreiber <code-schreiber@users.noreply.github.com>
2018-02-15 16:24:54 +0100

    Logging SecurityException instead of crashing

--- QuickCode/mobile/src/main/java/com/toolslab/quickcode/util/UriUtils.java
+++ QuickCode/mobile/src/main/java/com/toolslab/quickcode/util/UriUtils.java
@@ -27,6 +27,7 @@ import java.util.List;
 import javax.annotation.Nonnull;
 
 
+@SuppressWarnings("WeakerAccess") // Public methods need to be available as a utility class
 public class UriUtils {
 
     public static final String MODE_READ = "r";
@@ -107,24 +108,28 @@ public class UriUtils {
         // The query, since it only applies to a single document, will only return
         // one row. There's no need to filter, sort, or select fields, since we want
         // all fields for one document.
-        Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
-        if (cursor != null) {
-            if (cursor.getCount() > 1) {
-                Logger.logWarning(cursor.getCount() + " items in cursor!");
-            }
-            try {
-                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-                // "if there's anything to look at, look at it" conditionals.
-                if (cursor.moveToFirst()) {
-
-                    // Note it's called "Display Name".  This is
-                    // provider-specific, and might not necessarily be the file name.
-                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                    return cursor.getString(nameIndex);
+        try {
+            Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
+            if (cursor != null) {
+                if (cursor.getCount() > 1) {
+                    Logger.logWarning(cursor.getCount() + " items in cursor!");
                 }
-            } finally {
-                cursor.close();
+                try {
+                    // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+                    // "if there's anything to look at, look at it" conditionals.
+                    if (cursor.moveToFirst()) {
+
+                        // Note it's called "Display Name".  This is
+                        // provider-specific, and might not necessarily be the file name.
+                        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        return cursor.getString(nameIndex);
+                    }
+                } finally {
+                    cursor.close();
+                }
             }
+        } catch (SecurityException e) {
+            Logger.logException(e);
         }
         Logger.logWarning("Couldn't get display name from Uri: " + uri);
         return "";
@@ -132,29 +137,33 @@ public class UriUtils {
 
     @CheckResult
     public static int getSizeInBytes(ContentResolver contentResolver, Uri uri) {
-        Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
-        if (cursor != null) {
-            if (cursor.getCount() > 1) {
-                Logger.logWarning(cursor.getCount() + " items in cursor!");
-            }
-            try {
-                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-                // "if there's anything to look at, look at it" conditionals.
-                if (cursor.moveToFirst()) {
-                    int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
-                    // If the size is unknown, the value stored is null.  But since an
-                    // int can't be null in Java, the behavior is implementation-specific,
-                    // which is just a fancy term for "unpredictable".  So as
-                    // a rule, check if it's null before assigning to an int.  This will
-                    // happen often:  The storage API allows for remote files, whose
-                    // size might not be locally known.
-                    if (!cursor.isNull(sizeIndex)) {
-                        return cursor.getInt(sizeIndex);
-                    }
+        try {
+            Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
+            if (cursor != null) {
+                if (cursor.getCount() > 1) {
+                    Logger.logWarning(cursor.getCount() + " items in cursor!");
                 }
-            } finally {
-                cursor.close();
+                try {
+                    // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+                    // "if there's anything to look at, look at it" conditionals.
+                    if (cursor.moveToFirst()) {
+                        int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                        // If the size is unknown, the value stored is null.  But since an
+                        // int can't be null in Java, the behavior is implementation-specific,
+                        // which is just a fancy term for "unpredictable".  So as
+                        // a rule, check if it's null before assigning to an int.  This will
+                        // happen often:  The storage API allows for remote files, whose
+                        // size might not be locally known.
+                        if (!cursor.isNull(sizeIndex)) {
+                            return cursor.getInt(sizeIndex);
+                        }
+                    }
+                } finally {
+                    cursor.close();
+                }
             }
+        } catch (SecurityException e) {
+            Logger.logException(e);
         }
         Logger.logWarning("Couldn't get file size from Uri: " + uri);
         return SIZE_UNKNOWN;
@@ -162,14 +171,18 @@ public class UriUtils {
 
     @CheckResult
     public static boolean fileExists(ContentResolver contentResolver, Uri uri) {
-        Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                cursor.close();
-                return true;
-            } else {
-                cursor.close();
+        try {
+            Cursor cursor = contentResolver.query(uri, null, null, null, null, null);
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    cursor.close();
+                    return true;
+                } else {
+                    cursor.close();
+                }
             }
+        } catch (SecurityException e) {
+            Logger.logException(e);
         }
         return false;
     }
